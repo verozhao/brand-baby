0d6f842d5f81824ddb905c6a6d4ca232d283cfb6
verozhao <zz2906@nyu.edu>
2025-02-06 08:52:51 -0500

    update

--- new_app.py
+++ new_app.py
@@ -1,5 +1,3 @@
-# import runway
-# how to tell pydub fully qualified path name
 import io
 import re
 from openai import OpenAI
@@ -10,13 +8,9 @@ from flask import Flask, render_template, request, redirect, url_for
 import json
 
 
-openai_key = '' # Replace this with your own API
+openai_key = '' # Replace this with your own OpenAI API key
 
 client = OpenAI(api_key=openai_key)
-
-# [os.remove(os.path.join('#reels', f)) for f in os.listdir('#reels') if os.path.isfile(os.path.join('#reels', f))]
-
-# model = runway.Model(url='https://api.runwayml.com/v1/models/stable-diffusion-v1-5')
 
 app = Flask(__name__)
 
@@ -59,9 +53,7 @@ def calculate():
         if category == 'genre':
             print(f'Genre: {value}')
             genres.append(value)
-            # display:
-            # top creators, common words
-            # can do? related hashtags
+
         else:
             print(f'Link: {value}')
             match = re.search(r"v=([A-Za-z0-9_-]+)", value)
@@ -78,13 +70,6 @@ def calculate():
             transcripts.append(transcript)
             images.append(thumbnail)
             import requests; open(f"{video_id}_thumbnail.jpg", "wb").write(requests.get(f"https://img.youtube.com/vi/{video_id}/maxresdefault.jpg").content)
-
-            # base64_thumbnail = process_thumbnail(video_id)
-            
-        # elif category == 'username':
-        #     print(f'Username: {value}')
-        
-        # encoded_images = [encode_image(img_path) for img_path in images]
 
     if not sub_flag:
         text_prompt = str(text_input) + "Make a new video idea based on combining the following ideas, comments, and other videos:" + str(genres) + str(transcripts) + str(commentss)
@@ -127,22 +112,6 @@ def calculate():
     
     script = response2.choices[0].message.content
 
-    # user_message4 = [{
-    #     "role": "user",
-    #     "content": [
-    #         {"type": "text", "text": f"As an influencer, give a list of brands I could work with if I make this kind of YouTube video: {response.choices[0].message.content}"},
-    #     ]
-    # }]
-    
-    # response4 = client.chat.completions.create(
-    #     model="gpt-4o",
-    #     messages= user_message4,
-    #     max_tokens=200
-    # )
-    
-    # brands_list = response4.choices[0].message.content
-    # print(f"brands list {brands_list}")
-
     brands_list = "Museum of Flight, Boeing, Airbus, Lockheed Martin, Northrop Grumman, Raytheon Technologies, SpaceX, Blue Origin, Sierra Nevada Corporation, Honeywell Aerospace, GE Aerospace, Rolls-Royce Aerospace, Pratt & Whitney, Safran, BAE Systems, Thales Group, Virgin Galactic, Rocket Lab, L3Harris Technologies, Bell Helicopter, Embraer"
 
     text = f"Make a YouTube thumbnail for this: {response.choices[0].message.content}. No text!"
@@ -155,29 +124,6 @@ def calculate():
     )
     image_url = response3.data[0].url
     print(image_url)
-    # print('test')
-    # API_KEY = "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"
-    # API_HOST = 'https://api.stability.ai'
-    
-    # url = f"{API_HOST}/v1/generation/stable-diffusion-xl-1024-v1-0/image-to-image"
-    # headers = {
-    #     "Accept": "application/json",
-    #     "Authorization": f"Bearer {API_KEY}"
-    # }
-    # payload = {
-    #     "text_prompts": [{"text": f"Make a new thumbnail that is about {response.choices[0].message.content}." }],
-    #     "image_strength": 0.35,
-    #     "steps": 50,
-    # }
-
-    # response = requests.post(url, headers=headers, json=payload)
-
-    # data = response.json()
-    # print(data)
-    # image_data = base64.b64decode(data["artifacts"][0]["base64"])
-    
-    # image = Image.open(io.BytesIO(data))
-    # image.save("generated_image.png")
     
     print(input_dict)    
     return render_template('result.html', equation=equation, inputs=categorized_inputs, 
